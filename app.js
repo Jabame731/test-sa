2810bf88f16e0b7e1a3244e5e830d9c24792d4b5
jabame <jabameyumiko@gmail.com>
2022-04-19 17:50:33 +0800

    securtiy features

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+
 
 const User = new mongoose.model('User', userSchema);
 
@@ -45,7 +45,7 @@ app.post('/register', function(req, res){
 
     const NewUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     NewUser.save(function(err){
@@ -62,7 +62,7 @@ app.post('/register', function(req, res){
 app.post('/login', function(req, res){
 
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
 
     User.findOne({email: username}, function(err, foundUser){
